08ec865ecc329b502927afdef9fc0768a3bc4e91
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-09-09 15:46:13 -0300

    ..

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 
 from django.urls import path, include
-
+from django.contrib import admin
 urlpatterns = [
     path('', include('contact.urls')),
-
+    path('admin/', admin.site.urls),
 ]
