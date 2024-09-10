5ad1aaba3cf6f7521d8af1e0c41a33027b72f26d
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-09-10 15:23:19 -0300

    customizing the contact list dashboard in the admin area

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    # display of items in the admin page
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+
+    # ORDER BY:
+    # (if ordering = '-id', ORDER BY id DESC)
+    ordering = 'id',
+
+    # search bar (response if search is in items above)
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 200
+
+    # show which values are editable on the main dashboard without having to
+    #  enter the specific contact to edit.
+    # list_editable = 'first_name','last_name'
+
+    list_display_links = 'id', 'first_name',
+    # list_filter = ('created_date',)
