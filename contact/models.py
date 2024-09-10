4567fe77cccd62e5f11748f66dad678cb7fd801c
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-09-10 14:49:59 -0300

    creating a table of database and learning about migrations

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import django.utils.timezone as tz
 
-# Create your models here.
+
+class Contact(models.Model):
+    # models.CharField(max_length=50) -> string
+    # models.EmailField(max_length=254) -> type: email
+
+    # blank = True -> void fields are enabled
+    # 60 because the longest name has 58 letters
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60, blank=True)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=tz.now)
+    description = models.TextField(blank=True)
