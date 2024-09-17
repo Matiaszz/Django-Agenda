f6d220bb93a109700d608b026682c168d1103368
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-09-17 14:16:43 -0300

    Learning about add_error and validate a field

--- contact/forms.py
+++ contact/forms.py
@@ -5,16 +5,55 @@ from django.core.exceptions import ValidationError
 
 
 class ContactForm(forms.ModelForm):
+
+    first_name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'placeholder': 'This is forms.charfield'
+        }),
+        label='Your first name',
+        help_text='A help text for your user'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'placeholder': This is INIT',
+        #     'class': 'class1 class2',
+        # }
+        # )
+
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name', 'phone')
+        fields = ('first_name', 'last_name', 'email', 'phone')
+        # widgets = {
+        #     'first_name': forms.TextInput(attrs={
+        #         'class': 'classeaA classeB',
+        #         'placeholder': 'This is widgets'
+        #     }),
+        #     'email': forms.EmailInput()
+        # }
 
     def clean(self) -> dict[str, Any]:
-        # cleaned_data = self.cleaned_data
-        self.add_error('first_name', ValidationError(
-            'Mensagem de erro', code='invalid'
-        ))
-        self.add_error('first_name', ValidationError(
-            'Mensagem de erro 2', code='invalid'
-        ))
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
+        msg_error_duplicate_value = ValidationError(
+            'Não pode ser igual',)
+
+        if first_name == last_name:
+
+            self.add_error('first_name', msg_error_duplicate_value
+                           )
+
+            self.add_error('last_name', msg_error_duplicate_value
+                           )
         return super().clean()
+
+    # def clean_name_of_field
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'ABC':
+            self.add_error('first_name', ValidationError('Não pode ABC'))
+
+        return first_name
